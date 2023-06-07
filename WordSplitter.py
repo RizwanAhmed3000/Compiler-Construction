24d5d93853b4ac79614b4274655fb6c672f011a0
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-06-07 18:02:39 +0500

    Code refactored

--- WordSplitter.py
+++ WordSplitter.py
@@ -1,58 +1,211 @@
+# punctuators = [';', ',', ':', '[', ']', '{', '}', '(', ')']
+# operators = ['*', '/', '%', '+', '-', '<', '>', '&', 'or', 'not', '>=', '<=', '!=', '==', '++', '+=', '=','\\']
+# keywords = ['boolean','char','float','int','string','agar','warna','elif','torna,''jabtak','ke liye',
+#                 'private','protected','public','static','class','naya','khaali','dar-amad','tawseeh',
+#                 'supar','wapis','and','or','not']
+
+
+# def split_and_read_file(filename):
+#     characters = []  # List to store the lexemes
+#     character_count = 0  # Counter for total characters appended
+#     inside_comment = False  # Flag to track if inside a comment
+#     current_lexeme = ''  # Variable to store the current lexeme
+
+#     with open(filename, 'r') as file:
+#         current_char = file.read(1)  # Read the first character from the file
+
+#         while current_char:  # Continue reading until end of file
+#             dot_count=0
+            
+#             if inside_comment:
+#                 if current_char == '\n':  # If inside a comment, ignore characters until newline
+#                     inside_comment = False
+#                 current_char = file.read(1)  # Read the next character
+#                 continue
+
+#             elif current_char.isspace():  # If the character is whitespace
+#                 if current_lexeme:  # If there are previous characters, append the current lexeme
+#                     characters.append(current_lexeme)
+#                     character_count += len(current_lexeme)
+#                     current_lexeme = ''  # Reset the current lexeme
+#                 characters.append(current_char)  # Append the whitespace character
+#                 character_count += 1
+#                 current_char = file.read(1)  # Read the next character
+#                 continue
+
+#             elif current_char in punctuators:  # If the character is a punctuator
+#                 if current_lexeme:  # If there are previous characters, append the current lexeme
+#                     characters.append(current_lexeme)
+#                     character_count += len(current_lexeme)
+#                     current_lexeme = ''  # Reset the current lexeme
+#                 characters.append(current_char)
+#                 character_count += 1
+#                 current_char = file.read(1)  # Read the next character
+#                 continue
+
+#             elif current_char in operators:  # If the character is an operator
+#                 if current_lexeme:  # If there are previous characters, append the current lexeme
+#                     characters.append(current_lexeme)
+#                     character_count += len(current_lexeme)
+#                     current_lexeme = ''  # Reset the current lexeme
+
+#                 token = current_char
+#                 i = character_count  # Track the index of the current character
+
+#                 # Concatenate consecutive operators
+#                 while current_char := file.read(1):
+#                     if token + current_char in operators:
+#                         token += current_char
+#                     else:
+#                         break
+
+#                 characters.append(token)
+#                 character_count += len(token)
+#                 continue
+
+#             elif current_char == '\n':  # If the character is a newline
+#                 if current_lexeme:  # If there are previous characters, append the current lexeme
+#                     characters.append(current_lexeme)
+#                     character_count += len(current_lexeme)
+#                     current_lexeme = ''  # Reset the current lexeme
+#                 characters.append(current_char)  # Append the newline character
+#                 character_count += 1
+#                 current_char = file.read(1)  # Read the next character
+#                 continue
+
+#             elif current_char.isalpha() or current_char == '_':  # If the character is an alphabetic character or underscore
+#                 if current_lexeme and not current_lexeme.isdigit():  # If there are previous characters and not a digit
+#                     characters.append(current_lexeme)
+#                     character_count += len(current_lexeme)
+#                     current_lexeme = ''  # Reset the current lexeme
+
+#                 current_lexeme += current_char
+#                 current_char = file.read(1)  # Read the next character
+                
+#                 while current_char.isalpha()  or current_char == '_':
+#                     current_lexeme += current_char
+#                     current_char = file.read(1)  # Read the next character
+
+#                 continue
+
+#             elif current_char == '#':  # If the character is a hash (#) indicating a comment
+#                 inside_comment = True
+#                 current_char = file.read(1)  # Read the next character
+#                 continue
+#             elif current_char.isdigit( ) :  # If the character is a digit or a decimal point
+                
+#                 if current_lexeme:  # If there are previous characters, append the current lexeme
+#                     characters.append(current_lexeme)
+#                     character_count += len(current_lexeme)
+#                     current_lexeme = ''  # Reset the current lexeme
+                
+#                 if current_char == '.'  :
+#                     dot_count+=1
+                           
+#                     current_lexeme += current_char
+#                     current_char = file.read(1)  # Read the next character
+                    
+#                     while current_char.isdigit():
+#                         current_lexeme += current_char
+#                         current_char = file.read(1)  # Read the next character
+
+
+#                 else:
+#                     print(current_char,"  ",dot_count,"  ",current_lexeme)
+                        
+#                     current_lexeme += current_char
+#                     current_char = file.read(1)  # Read the next character
+
+#                     while (current_char.isdigit() or current_char == '.')  :
+#                         if current_char == '.':
+#                             if dot_count==1:
+#                                 characters.append(current_lexeme)
+#                                 character_count += len(current_lexeme)
+#                                 current_lexeme = ''
+#                                 print(current_char,"  ",dot_count,"  ",current_lexeme)
+#                                 break
+                                
+                            
+#                             dot_count+=1
+#                             current_lexeme += current_char
+#                             current_char = file.read(1)  # Read the next character
+#                         else:
+#                             current_lexeme += current_char
+#                             current_char = file.read(1)  # Read the next character
+
+#                 continue
+
+#             current_lexeme += current_char
+#             current_char = file.read(1)  # Read the next character
+
+#     if current_lexeme:
+#         characters.append(current_lexeme)
+
+#     return characters
+
+
+# filename = 'hello.txt'
+# lexemes = split_and_read_file(filename)
+# print(lexemes)
+
+
+# Refactor
+
 punctuators = [';', ',', ':', '[', ']', '{', '}', '(', ')']
-operators = ['*', '/', '%', '+', '-', '<', '>', '&', 'or', 'not', '>=', '<=', '!=', '==', '++', '+=', '=','\\']
-keywords = ['boolean','char','float','int','string','agar','warna','elif','torna,''jabtak','ke liye',
-                'private','protected','public','static','class','naya','khaali','dar-amad','tawseeh',
-                'supar','wapis','and','or','not']
+operators = ['*', '/', '%', '+', '-', '<', '>', '&', 'or',
+             'not', '>=', '<=', '!=', '==', '++', '+=', '=', '\\']
+keywords = ['boolean', 'char', 'float', 'int', 'string', 'agar', 'warna', 'elif', 'torna', 'jabtak', 'ke liye',
+            'private', 'protected', 'public', 'static', 'class', 'naya', 'khaali', 'dar-amad', 'tawseeh',
+            'supar', 'wapis', 'and', 'or', 'not']
 
 
 def split_and_read_file(filename):
-    characters = []  # List to store the lexemes
-    character_count = 0  # Counter for total characters appended
-    inside_comment = False  # Flag to track if inside a comment
-    current_lexeme = ''  # Variable to store the current lexeme
+    characters = []
+    character_count = 0
+    inside_comment = False
+    current_lexeme = ''
 
     with open(filename, 'r') as file:
-        current_char = file.read(1)  # Read the first character from the file
+        current_char = file.read(1)
 
-        while current_char:  # Continue reading until end of file
-            dot_count=0
-            
+        while current_char:
+            dot_count = 0
+
             if inside_comment:
-                if current_char == '\n':  # If inside a comment, ignore characters until newline
+                if current_char == '\n':
                     inside_comment = False
-                current_char = file.read(1)  # Read the next character
+                current_char = file.read(1)
                 continue
 
-            elif current_char.isspace():  # If the character is whitespace
-                if current_lexeme:  # If there are previous characters, append the current lexeme
+            elif current_char.isspace():
+                if current_lexeme:
                     characters.append(current_lexeme)
                     character_count += len(current_lexeme)
-                    current_lexeme = ''  # Reset the current lexeme
-                characters.append(current_char)  # Append the whitespace character
-                character_count += 1
-                current_char = file.read(1)  # Read the next character
-                continue
-
-            elif current_char in punctuators:  # If the character is a punctuator
-                if current_lexeme:  # If there are previous characters, append the current lexeme
-                    characters.append(current_lexeme)
-                    character_count += len(current_lexeme)
-                    current_lexeme = ''  # Reset the current lexeme
+                    current_lexeme = ''
                 characters.append(current_char)
                 character_count += 1
-                current_char = file.read(1)  # Read the next character
+                current_char = file.read(1)
                 continue
 
-            elif current_char in operators:  # If the character is an operator
-                if current_lexeme:  # If there are previous characters, append the current lexeme
+            elif current_char in punctuators:
+                if current_lexeme:
                     characters.append(current_lexeme)
                     character_count += len(current_lexeme)
-                    current_lexeme = ''  # Reset the current lexeme
+                    current_lexeme = ''
+                characters.append(current_char)
+                character_count += 1
+                current_char = file.read(1)
+                continue
+
+            elif current_char in operators:
+                if current_lexeme:
+                    characters.append(current_lexeme)
+                    character_count += len(current_lexeme)
+                    current_lexeme = ''
 
                 token = current_char
-                i = character_count  # Track the index of the current character
+                i = character_count
 
-                # Concatenate consecutive operators
                 while current_char := file.read(1):
                     if token + current_char in operators:
                         token += current_char
@@ -63,80 +216,75 @@ def split_and_read_file(filename):
                 character_count += len(token)
                 continue
 
-            elif current_char == '\n':  # If the character is a newline
-                if current_lexeme:  # If there are previous characters, append the current lexeme
+            elif current_char == '\n':
+                if current_lexeme:
                     characters.append(current_lexeme)
                     character_count += len(current_lexeme)
-                    current_lexeme = ''  # Reset the current lexeme
-                characters.append(current_char)  # Append the newline character
+                    current_lexeme = ''
+                characters.append(current_char)
                 character_count += 1
-                current_char = file.read(1)  # Read the next character
+                current_char = file.read(1)
                 continue
 
-            elif current_char.isalpha() or current_char == '_':  # If the character is an alphabetic character or underscore
-                if current_lexeme and not current_lexeme.isdigit():  # If there are previous characters and not a digit
+            elif current_char.isalpha() or current_char == '_':
+                if current_lexeme and not current_lexeme.isdigit():
                     characters.append(current_lexeme)
                     character_count += len(current_lexeme)
-                    current_lexeme = ''  # Reset the current lexeme
+                    current_lexeme = ''
 
                 current_lexeme += current_char
-                current_char = file.read(1)  # Read the next character
-                
-                while current_char.isalpha()  or current_char == '_':
+                current_char = file.read(1)
+
+                while current_char.isalpha() or current_char == '_':
                     current_lexeme += current_char
-                    current_char = file.read(1)  # Read the next character
+                    current_char = file.read(1)
 
                 continue
 
-            elif current_char == '#':  # If the character is a hash (#) indicating a comment
+            elif current_char == '#':
                 inside_comment = True
-                current_char = file.read(1)  # Read the next character
+                current_char = file.read(1)
                 continue
-            elif current_char.isdigit( ) :  # If the character is a digit or a decimal point
-                
-                if current_lexeme:  # If there are previous characters, append the current lexeme
+
+            elif current_char.isdigit():
+                if current_lexeme:
                     characters.append(current_lexeme)
                     character_count += len(current_lexeme)
-                    current_lexeme = ''  # Reset the current lexeme
-                
-                if current_char == '.'  :
-                    dot_count+=1
-                           
+                    current_lexeme = ''
+
+                if current_char == '.':
+                    dot_count += 1
+
                     current_lexeme += current_char
-                    current_char = file.read(1)  # Read the next character
-                    
+                    current_char = file.read(1)
+
                     while current_char.isdigit():
                         current_lexeme += current_char
-                        current_char = file.read(1)  # Read the next character
-
+                        current_char = file.read(1)
 
                 else:
-                    print(current_char,"  ",dot_count,"  ",current_lexeme)
-                        
                     current_lexeme += current_char
-                    current_char = file.read(1)  # Read the next character
+                    current_char = file.read(1)
 
-                    while (current_char.isdigit() or current_char == '.')  :
+                    while current_char.isdigit() or current_char == '.':
                         if current_char == '.':
-                            if dot_count==1:
+                            if dot_count == 1:
                                 characters.append(current_lexeme)
                                 character_count += len(current_lexeme)
                                 current_lexeme = ''
-                                print(current_char,"  ",dot_count,"  ",current_lexeme)
                                 break
-                                
-                            
-                            dot_count+=1
+
+                            dot_count += 1
                             current_lexeme += current_char
-                            current_char = file.read(1)  # Read the next character
+                            current_char = file.read(1)
                         else:
                             current_lexeme += current_char
-                            current_char = file.read(1)  # Read the next character
+                            current_char = file.read(1)
 
                 continue
 
             current_lexeme += current_char
-            current_char = file.read(1)  # Read the next character
+            current_char = file.read(1)
 
     if current_lexeme:
         characters.append(current_lexeme)
